c06f923060dd11e0f5e0707b525992e6ea86ae0b
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-03-09 10:42:49 -0700

    sidebar complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { BsBag } from "react-icons/bs";
+
 const Header = () => {
+
+    const {isOpen, setIsOpen} = useContext(SidebarContext)
     return(
         <div>
             <div>Header</div>
-            <div>open/close sidebar</div>
+            <div onClick={() => setIsOpen(!isOpen)} className="cursor-pointer flex relative ">
+                <BsBag className="text-2xl" />
+            </div>
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,21 @@
 import { useContext } from "react";
-import { SidebarContext } from "../contexts/SidebarContext";
+import SidebarProvider, { SidebarContext } from "../contexts/SidebarContext";
+import { IoMdArrowForward } from "react-icons/io";
 
 const Sidebar = () => {
 
-    const [isOpen, handleClose] = useContext(SidebarContext); 
+    const {isOpen, handleClose} = useContext(SidebarContext); 
     return(
-        <div className={` ${isOpen ? 'right-0' : 'right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] 
-        transition-all duration-300 z-20 px-4 lg:px-[35px]`}>Sidebar</div>
+        <div className={` ${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] 
+        transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+            <div className="flex justify-between items-center py-6 border-b">
+                <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+                {/* icon  */}
+                <div onClick={handleClose} className="cursor-pointer w-8 h-8 flex justify-center items-center">
+                    <IoMdArrowForward className="text-2xl" />
+                </div>
+            </div>
+        </div>
     )
 }
 
