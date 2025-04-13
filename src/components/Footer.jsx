b435cd6d01099f9029d8d22f7e8701927b3d9c0d
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-04-13 12:22:45 -0700

    some changhes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ const Footer = () => {
     return(
         <footer className="bg-black py-6">
             <div className="mx-auto">
-                <p className="text-white text-center">Copyright &copy; Shop 2025</p>
+                <p className="text-white text-center underline font-semibold"><a href="https://mmdrahimi-resume.my.canva.site/">Creator Resume</a></p>
             </div>
         </footer>
     )
 }
 
 export default Footer;
+// Copyright &copy; Shop 2025
