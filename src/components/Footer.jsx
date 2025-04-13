45977c13b3c3bbc331908b31e3836db7a180a735
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-04-13 13:04:59 -0700

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 const Footer = () => {
-    return(
-        <footer className="bg-black py-6">
-            <div className="mx-auto">
-                <p className="text-white text-center underline font-semibold"><a href="https://mmdrahimi-resume.my.canva.site/">Creator Resume</a></p>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="bg-black py-6">
+      <div className="mx-auto">
+        Copyright &copy; Shop 2025
+        <p className="text-white text-center underline font-semibold"></p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
-// Copyright &copy; Shop 2025
+{
+  /* <a href="https://mmdrahimi-resume.my.canva.site/">Creator Resume</a> */
+}
